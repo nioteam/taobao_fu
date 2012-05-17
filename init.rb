5facee21b3e6a9b798af64ef8335b8a9131d4744
Leslie Zhao <zxl51@qq.com>
2012-05-17 17:25:20 +0800

    Fix "couldn't parse YAML" error ruby 1.9.2/rails 3.1

--- init.rb
+++ init.rb
@@ -1,4 +1,8 @@
 require "taobao_fu"
 
+#Fix "couldn't parse YAML" error ruby 1.9.2/rails 3.1
+require 'yaml'
+YAML::ENGINE.yamler= 'syck'
+
 config_file = File.join(Rails.root, "config", "taobao.yml")
 TaobaoFu.load(config_file) if FileTest::exists?(config_file)
